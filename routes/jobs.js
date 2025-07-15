514ee589b24b4c982d55288db8cfeeba1f52e5a4
bezerragb <bezerragb01@gmail.com>
2025-07-15 19:21:02 -0300

    feat: adiciona rota para exibir vaga por ID e view de detalhes da vaga

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const router = express.Router();
-const job = require('../models/Job')
+const job = require('../models/Job');
+
 
 
 router.get('/add', (req, res) => {
     res.render('add')
 })
+
+//rota para buscar o job atraves do ID
+router.get('/view/:id', (req, res) => job.findOne ({
+    where: {id: req.params.id}
+}).then(job => {
+    res.render ('view', {
+        job
+    })
+}).catch(err => console.log(err)))
+
 
 // rota criada para fazer teste via postman
 router.get('/test', (req, res) => {
